03b4e97cc1fe8f4141713f55582b50a448553947
AlanF-MW <103284994+alanfmw@users.noreply.github.com>
2022-06-28 17:32:41 -0400

    changed version to 9.11.15a0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class _MatlabFinder(build_py):
     MATLAB_REL = 'R2021b'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-    MATLAB_VER = '9.11.14'
+    MATLAB_VER = '9.11.15a0'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
     SUPPORTED_PYTHON_VERSIONS = set(['3.7', '3.8', '3.9'])
@@ -299,7 +299,7 @@ if __name__ == '__main__':
     setup(
         name="matlabengine",
         # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-        version="9.11.14",
+        version="9.11.15a0",
         description='A module to call MATLAB from Python',
         author='MathWorks',
         license="MathWorks XSLA License",
